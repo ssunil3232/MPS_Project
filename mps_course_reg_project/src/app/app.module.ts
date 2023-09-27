44dbaa3fb3aaf0b7067eaf614e87a52b17b8d253
“sunil3232” <soniasunil47@gmail.com>
2023-09-27 16:36:09 -0400

    login and register components

--- mps_course_reg_project/src/app/app.module.ts
+++ mps_course_reg_project/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ButtonModule } from 'primeng/button';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TokenInterceptor } from '../authentication/helpers/token.interceptor';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { CourseCatalogComponent } from './course-catalog/course-catalog.component';
 import { SchedulerComponent } from './scheduler/scheduler.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { StudentInformationComponent } from './student-information/student-information.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
 
 @NgModule({
   declarations: [
@@ -17,14 +25,24 @@ import { StudentInformationComponent } from './student-information/student-infor
     CourseCatalogComponent,
     SchedulerComponent,
     RegistrationComponent,
-    StudentInformationComponent
+    StudentInformationComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
+    FormsModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    ButtonModule
+    ButtonModule,
+    InputTextModule,
+    PasswordModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
